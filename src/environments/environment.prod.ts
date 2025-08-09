7267418c972756b41e3e2e5d9aaea2b69b0d393d
filipwasiewicz <filip.wasiewcz11@gmail.com>
2025-08-09 17:01:12 +0200

    add environment for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://adopsiak-erg8bucjgefbaghm.polandcentral-01.azurewebsites.net/api'
+  apiUrl: 'https://adopsiak-erg8bucjgefbaghm.polandcentral-01.azurewebsites.net'
 };
